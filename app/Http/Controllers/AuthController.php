d683f954be792fc932e31c72334421e3b6c9fd44
Teatoller <steven.otieno1@gmail.com>
2020-01-02 12:23:00 +0300

    (passport): Implement passport
    - initial commit
    - install passport
    - update schema design
    - update user model
    - update controller
    - add hasAPITokens in user model
    - add passport routes in provider
    - add passport driver to auth config
    - add auth route
    - add register method in controller
    - add email to validation rule
    
    [Finishes #001]

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validateData = $request->validate([
+            'firstname' => 'required|max:55',
+            'lastname' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        $validateData['password'] = bcrypt($validateData['password']);
+
+        $user = User::create($validateData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response(['user' => $user, 'access_token' => $accessToken]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid credentials']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
